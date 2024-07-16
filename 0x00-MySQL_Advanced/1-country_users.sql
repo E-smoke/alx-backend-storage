95c588b13fb776cda4ac2a6692095b34e5381d1a
Elijah <eachala5@gmail.com>
2024-07-16 01:44:43 +0000

    002

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,9 @@
 --create table users
-
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    country ENUM('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id)
 );
--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
 --sql statement
-SELECT origin AS origin, SUM(fans) AS nb_fans FROM metal_bands
+SELECT origin AS origin, SUM(fans) AS nb_fans
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
