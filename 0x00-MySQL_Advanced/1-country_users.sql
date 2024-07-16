18bbc6e94b452a5ce941678529df9d25da22af8c
Elijah <eachala5@gmail.com>
2024-07-16 01:14:07 +0000

    1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+--create table users
+
+DROP TABLE IF users IF EXIST;
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
